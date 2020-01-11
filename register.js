6c53753d2dfe782d6040c76f3c2ed2996c0d6646
daudcanugerah <daudcanugerah@gmail.com>
2020-01-12 01:22:22 +0700

    split route,global setting and main server

---
+++ register.js
@@ -0,0 +1,31 @@
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+const { join } = require('path')
+
+const Acl = require('./app/helpers/ValidationHelper')
+const access = require('./app/helpers/acl/acl.json')
+
+module.exports = (app, express) => {
+  /**
+     *  Register Global Variabel
+     */
+  app.locals.Acl = new Acl(access)
+
+  /**
+     *  Register Middleware
+     */
+  app.use(cookieParser()) // using cookie parser
+  app.use(express.static('public', {}))
+  app.use(express.json()) // for parsing application/json
+  app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+  /**
+     *  Load Morgan
+     *  Desc : HTTP request logger middleware for node.js
+     */
+  app.use(morgan('combined'))
+
+  app.set('view engine', 'pug')
+  app.set('views', join(__dirname, 'app/views'))
+
+  return app
+}
---
+++ route.js
@@ -0,0 +1,20 @@
+const backendRoutes = require('./app/routes/backendRoutes')
+const frontendRoutes = require('./app/routes/frontendRoutes')
+
+module.exports = (app) => {
+  app.use('/api', backendRoutes)
+  app.use('/', frontendRoutes)
+  app.use((err, req, res, next) => {
+    res.status((err && err.data && err.data.code) || 500)
+    res.json(err)
+  })
+
+  //   app.all('*', (req, res) => {
+  //     res.status(404).send({
+  //       status: 404,
+  //       version: '0.1.0',
+  //       message: 'Path Not Found'
+  //     })
+  //   })
+  return app
+}
