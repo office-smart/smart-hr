29dccd8332f2191f08ef3a3d38906fcc5ddca771
daudcanugerah <daudcanugerah@gmail.com>
2020-01-12 01:26:14 +0700

    convert obj to class

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,39 +1,34 @@
-const userService = require('../services/userService')
-const errorHandler = require('../libs/errorHandler')
-const { canAccess } = require('../helpers/access')
+const userService = {}
+const { InternalError } = require('../libs/ErrorHandler')
 
-const errorMessage = function (obj = {}) {
-    errorHandler({...obj, controller: 'User'})
-    console.log(obj.err)
-}
-
-// initiate the controller
-let controller = {}
-
-controller.login = async (req, res, next) => {
+class UserController {
+  async login (req, res, next) {
     try {
-        const data = await userService.login(req.body)
-        req.app.locals.role.setRole(data.roleType)
-        res.send(data)
+      const data = await userService.login(req.body)
+      req.app.locals.role.setRole(data.roleType)
+      res.send(data)
     } catch (err) {
-        errorMessage({req, res, err})
+      new InternalError({ message: 'something error happen', stack: err.toString() })
     }
-}
-controller.getUsers = async (req, res, next) => {
+  }
+
+  async getUsers (req, res, next) {
     try {
-        const items = await userService.getUsers(req.query)
-        res.send({ access : req.app.locals.Acl.getAccess('user'), items })
+      const items = await userService.getUsers(req.query)
+      res.send({ access: req.app.locals.Acl.getAccess('user'), items })
     } catch (err) {
-        errorMessage({req, res, err})
+      new InternalError({ message: 'something error happen', stack: err.toString() })
     }
-}
-controller.create = async (req, res, next) => {
+  }
+
+  async create (req, res, next) {
     try {
-        const data = await userService.create(req.body)
-        res.send(data)
+      const data = await userService.create(req.body)
+      res.send(data)
     } catch (err) {
-        errorMessage({req, res, err})
+      new InternalError({ message: 'something error happen', stack: err.toString() })
     }
+  }
 }
 
-module.exports = controller
+module.exports = UserController
